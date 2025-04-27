33308eb3eb842bf475e008b5abe615ab2d745a41
Steve <stevedsimkins@gmail.com>
2025-04-27 12:32:19 -0400

    chore: Updated description and added framework cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,11 @@ import { ApiResponse } from 'shared'
 import { Button } from '@/components/ui/button'
 import { GithubIcon, ClipboardCopy, Check } from 'lucide-react'
 import { Link } from '@mini_apps/utilities'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import bun from "./assets/bun.svg"
+import vite from "./assets/vite.svg"
+import hono from "./assets/hono.svg"
+import react from "./assets/react.svg"
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
 
@@ -39,7 +44,7 @@ function App() {
           </div>
           <h1 className="text-4xl md:text-5xl font-bold">bhvr</h1>
           <p className="text-xl text-muted-foreground">Bun + Hono + Vite + React</p>
-          <p className="text-center max-w-md">A typesafe fullstack monorepo template for modern web applications</p>
+          <p className="text-center max-w-md">Modern and lightweight fullstack repo for building web applications in Typescript</p>
         </div>
 
         <div className="flex flex-wrap justify-center gap-4">
@@ -81,6 +86,94 @@ function App() {
             >
               {copied ? <Check className="h-4 w-4" /> : <ClipboardCopy className="h-4 w-4" />}
             </Button>
+          </div>
+        </div>
+
+        <div className="w-full mt-6">
+          <h2 className="text-xl font-semibold mb-4 text-center">Why bhvr?</h2>
+          <p>While there are plenty of existing app building stacks out there, many of them are either bloated, outdated, or have too much of a vendor lock-in. bhvr is built with the opinion that you should be able to deploy your client or server in any enviorment while also keeping type saftey.</p>
+          <br/>
+          <p>Each piece of the stack is lightweight and blazing fast to keep your web apps simple and portable. If you're not familiar with Bun, Hono, or Vite, they are replacements for the following:</p>
+          <br />
+          <ul className='pl-6 list-disc space-y-2'>
+            <li>Node / NPM -&gt; Bun</li>
+            <li>Express -&gt; Hono</li>
+            <li>Webpack -&gt; Vite</li>
+          </ul>
+        </div>
+
+        <div className="w-full mt-6">
+          <div className="grid grid-cols-1 gap-4">
+            <Link href="https://bun.sh">
+            <Card>
+              <CardHeader className="flex flex-row items-center gap-2">
+                <img src={bun} alt="Bun logo" className="w-8 h-8" />
+                <div>
+                  <CardTitle>Bun</CardTitle>
+                  <CardDescription>JavaScript runtime & toolkit</CardDescription>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm">
+                  A fast all-in-one JavaScript runtime with native bundler,
+                  test runner, and npm-compatible package manager. In bhvr Bun is used to install dependencies, bundle types, and manage the workspace.
+                </p>
+              </CardContent>
+            </Card>
+            </Link>
+
+            <Link href='https://hono.dev'>
+
+            <Card>
+              <CardHeader className="flex flex-row items-center gap-2">
+                <img src={hono} alt="Hono logo" className="w-8 h-8" />
+                <div>
+                  <CardTitle>Hono</CardTitle>
+                  <CardDescription>Ultrafast web framework</CardDescription>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm">
+                  Lightweight, ultrafast web framework for the Edges.
+                  With type safety built in and a minimal API. bhvr uses Hono for it's server making it dead simple to build your backend and API.
+                </p>
+              </CardContent>
+            </Card>
+            </Link>
+
+            <Link href="https://vite.dev">
+            <Card>
+              <CardHeader className="flex flex-row items-center gap-2">
+                <img src={vite} alt="Vite logo" className="w-8 h-8" />
+                <div>
+                  <CardTitle>Vite</CardTitle>
+                  <CardDescription>Next generation frontend tooling</CardDescription>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm">
+                  Provides instant server start, lightning-fast HMR,
+                  and optimized builds for production deployment. bhvr uses Vite for bundling the client frontend, and giving users a "just works" experience while also offering great ecosystem plugins.
+                </p>
+              </CardContent>
+            </Card>
+            </Link>
+            <Link href="https://react.dev">
+            <Card>
+              <CardHeader className="flex flex-row items-center gap-2">
+                <img src={react} alt="React logo" className="w-8 h-8" />
+                <div>
+                  <CardTitle>React</CardTitle>
+                  <CardDescription>UI component library</CardDescription>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm">
+                  A classic default for building frontend UIs, but can easily be swapped with something else.
+                </p>
+              </CardContent>
+            </Card>
+            </Link>
           </div>
         </div>
 
